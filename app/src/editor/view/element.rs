235c334a21f79290bcca1bb6868e8b4b6069675b
Gareth Cottrell <gareth@qltech.co.za>
2026-04-30 09:30:01 +0200

    strip(cleanup-10): remove voice_input from editor/view/element + caller
    - editor/view/element.rs: dead struct field, dead constructor param,
      dead maybe_handle_voice_toggle method, simplified key handler
    - editor/view/mod.rs: removed caller arg to EditorElement::new, deleted
      unused voice_input_toggle_key_code helper method, removed unused
      KeyCode import
    
    cargo check clean: zero errors, zero warnings.

--- app/src/editor/view/element.rs
+++ app/src/editor/view/element.rs
@@ -218,8 +218,6 @@ pub struct EditorElement {
     remote_selections_data: HashMap<ReplicaId, RemoteDrawableSelectionData>,
 
     voice_input_cursor_icon: Option<Box<dyn Element>>,
-    #[cfg_attr(not(feature = "voice_input"), allow(unused))]
-    voice_input_toggle_key_code: Option<KeyCode>,
 }
 
 impl EditorElement {
@@ -238,7 +236,6 @@ impl EditorElement {
         local_selection_data: LocalDrawableSelectionData,
         remote_selections_data: HashMap<ReplicaId, super::RemoteDrawableSelectionData>,
         cursor_display_type: Option<CursorDisplayType>,
-        voice_input_toggle_key_code: Option<KeyCode>,
     ) -> Self {
         let remote_selections_data = HashMap::from_iter(remote_selections_data.into_iter().map(
             |(replica_id, drawable_selections_data)| {
@@ -268,7 +265,6 @@ impl EditorElement {
             preferred_cursor_type: cursor_display_type.unwrap_or_default(),
             cycle_next_command_hint: None,
             voice_input_cursor_icon: None,
-            voice_input_toggle_key_code,
         }
     }
 
@@ -445,31 +441,8 @@ impl EditorElement {
         state: &KeyState,
         ctx: &mut EventContext,
     ) -> bool {
-        cfg_if::cfg_if! {
-            if #[cfg(feature = "voice_input")] {
-                self.maybe_handle_voice_toggle(key_code, state, ctx);
-            } else {
-                // Silence unused param warnings when voice_input is disabled.
-                let _ = (key_code, state, ctx);
-            }
-        }
+        let _ = (key_code, state, ctx);
         false
-    }
-
-    #[cfg(feature = "voice_input")]
-    fn maybe_handle_voice_toggle(
-        &self,
-        key_code: &KeyCode,
-        state: &KeyState,
-        ctx: &mut EventContext,
-    ) {
-        if let Some(voice_input_toggle_key_code) = self.voice_input_toggle_key_code {
-            if *key_code == voice_input_toggle_key_code {
-                ctx.dispatch_typed_action(EditorAction::ToggleVoiceInput(
-                    voice_input::VoiceInputToggledFrom::Key { state: *state },
-                ));
-            }
-        }
     }
 
     fn mouse_moved(
